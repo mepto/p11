898149e6d3e32f9015ee3221f92f993120d8eb73
mepto <natg.inf@gmail.com>
2023-05-21 22:00:04 +0200

    Use decode instead of encode in tests

--- app/tests/unit_tests/test_login.py
+++ app/tests/unit_tests/test_login.py
@@ -1,18 +1,16 @@
 from constants import EMAIL_ERROR
 from app.tests.data import DataForTests
 
-# TODO use decode on x.data instead of encode
-
 
 def test_login_unknown_email(client):
     """Check the system provide a human-readable error on unknown email."""
     response = client.post("/show_summary", data={"email": DataForTests.UNKNOWN_USER}, follow_redirects=True)
-    assert str.encode(EMAIL_ERROR) in response.data
+    assert EMAIL_ERROR in response.data.decode()
     assert response.status_code == 401
 
 
 def test_login_known_email(client):
     """Check the system sends a positive response when the email is known."""
     response = client.post("/show_summary", data={"email": DataForTests.VALID_USER}, follow_redirects=True)
-    assert str.encode(DataForTests.VALID_USER) in response.data
+    assert DataForTests.VALID_USER in response.data.decode()
     assert response.status_code == 200
